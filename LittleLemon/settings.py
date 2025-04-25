7c559ae884138a5b7a3af332e531bbd3c5998a32
ahmad <ahmadrgul@gmail.com>
2025-04-25 16:21:28 +0500

    setup booking and menu API

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Restaurant',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .models import MenuItem, Booking
+from .serializers import MenuItemSerializer, BookingSerializer
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
